9eb49ad26f2d1236465c111d77cfefe516749c5f
Admin <ujif987@mail.ru>
2022-03-05 13:30:37 +0300

    added config file

---
+++ config.py
@@ -0,0 +1,31 @@
+"""Main info"""
+bot_token = 'your vk bot token here'
+telegram_token = "your telegram token here"
+schedule_table = [[4, 11], [7, 11], [10, 4], [14, 13], [16, 42], [18, 34], [20, 11], [22, 17]]
+
+
+bot_group_id = 0
+album_id = 0
+
+"""
+Info for unittests
+"""
+vk_test_user_id = 0 # your test user id (vk)
+tg_test_user_id = 0 # your tg user id for tests (telegram)
+test_fold = 'users/test_user'
+
+
+
+"""
+Project info
+"""
+project_name = 'Auto-posting Bot'
+
+"""ps"""
+
+"""
+to get your token go to link
+https://oauth.vk.com/oauth/authorize?client_id=8035251&scope=photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,email,notifications,stats,ads,offline,docs,pages,stats,notifications&response_type=token&__q_hash=69940e59922728d45b3bca4d29ab1dc9
+or
+https://vkhost.github.io/
+"""
